de7a14aa86da24eba47e2578e4bbfde296653dc1
LuanLudwig <luantelisluh@gmail.com>
2023-11-07 18:57:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+/*componentes */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
+import Message from './components/layout/Message';
+import Home from './components/pages/Home';
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
+
+
+/* context */
+import { UserProvider } from './context/UserContext';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserProvider>
+        <Navbar />
+        <Message />
+          <Container>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </Container>
+        <Footer />
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import api from '../../utils/api';
+
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import styles from './Home.module.css';
+
+
+function Home() {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Home
